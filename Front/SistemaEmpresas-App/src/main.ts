10140b12b6bc4293256058e27e05543526662e60
douglasscdoug <douglasscdoug@gmail.com>
2026-06-28 16:04:42 -0300

    chore: adiciona suporte ao Vercel Analytics no projeto

--- Front/SistemaEmpresas-App/src/main.ts
+++ Front/SistemaEmpresas-App/src/main.ts
@@ -3,8 +3,10 @@ import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { inject } from "@vercel/analytics"
 
 registerLocaleData(localePt);
+inject();
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
